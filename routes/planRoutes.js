e6f320bd3bcb5a87df0aad038217a2b7dfeb79cc
minyoung <wainou.6@gmail.com>
2025-09-13 13:58:36 +0900

    [황민영] ✅ test : 통신사만 등록 가능
    [황민영] ✅ test : 통신사만 등록 가능

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const planController = require('../controllers/planController')
-const { isAuthenticated, isAdmin, isAgency } = require('../middlewares/authMiddleware')
+const { isAuthenticated, isAgency } = require('../middlewares/authMiddleware')
 const { upload } = require('../utils/fileUpload')
 
 router.post('/', isAuthenticated, isAgency, upload.array('images'), planController.createPlan)
--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const serviceController = require('../controllers/serviceController')
 const { isAuthenticated, isAgency } = require('../middlewares/authMiddleware')
 
-router.post('/', isAuthenticated, serviceController.createService)
+router.post('/', isAuthenticated, isAgency, serviceController.createService)
 
 module.exports = router

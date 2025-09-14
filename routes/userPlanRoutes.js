5337d664508da4ebdb2a2bea31b94816845bc1f1
minyoung <wainou.6@gmail.com>
2025-09-14 17:32:38 +0900

    [황민영] ✨ feat : 요금제 관련 구현
    [황민영] ✨ feat : 요금제 생성 할때 endDate도 추가, 자동 만료 구현과 내 요금제 조회, 청구서 조회 구현

--- routes/userPlanRoutes.js
+++ routes/userPlanRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { isAuthenticated, isAdmin } = require('../middlewares/authMiddleware')
+const { isAuthenticated } = require('../middlewares/authMiddleware')
 const { subscriptionLimiter } = require('../middlewares/rateLimiter')
 const userPlanController = require('../controllers/userPlanController')
 
 const env = process.env.NODE_ENV || 'development'
 
+// 내 요금제 목록 조회
+router.get('/', isAuthenticated, userPlanController.getMyUserPlans)
+// 내 요금제 청구서
+router.get('/bill', isAuthenticated, userPlanController.getMyUserPlanBill)
 // 사용자 플랜 생성 (구독 시작)
 router.post('/', subscriptionLimiter(env), isAuthenticated, userPlanController.createUserPlan)
 

b8c06c57ecbd5b4c52fefd60e8e9d1193197cce0
minyoung <wainou.6@gmail.com>
2025-09-19 10:27:49 +0900

    [황민영] ⚡ perf : 오류, info, 모두 logs 폴더 파일에 저장
    [황민영] ⚡ perf : 오류, info, 모두 logs 폴더 파일에 저장

--- utils/logger.js
+++ utils/logger.js
@@ -2,7 +2,7 @@ const winston = require('winston')
 const path = require('path')
 
 const logger = winston.createLogger({
-   level: 'error', // 에러 레벨만 로깅
+   level: 'info', // info 이상 모두 기록
    format: winston.format.combine(
       winston.format.timestamp({
          format: 'YYYY-MM-DD HH:mm:ss',
@@ -17,7 +17,12 @@ const logger = winston.createLogger({
          filename: path.join(__dirname, '../logs/error.log'),
          level: 'error',
       }),
-      // 콘솔 출력 (에러만)
+      // info 이상 로그는 combined.log 파일에도 저장
+      new winston.transports.File({
+         filename: path.join(__dirname, '../logs/combined.log'),
+         level: 'info',
+      }),
+      // 콘솔 출력 (info 이상)
       new winston.transports.Console({
          format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
          level: 'info',
